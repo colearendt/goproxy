6a039356b0cb71f458da12da080c11a4d6a1f6b6
Cole Arendt <cole.arendt@outlook.com>
2020-03-04 22:30:40 -0500

    move file

--- src/main.go
+++ src/main.go
@@ -96,12 +96,32 @@ func getProxyUrl(proxyConditionRaw string) string {
 	return default_condtion_url
 }
 
+// Serve a reverse proxy for a given url
+func serveReverseProxy(target string, res http.ResponseWriter, req *http.Request) {
+	// parse the url
+	url, _ := url.Parse(target)
+
+	// create the reverse proxy
+	proxy := httputil.NewSingleHostReverseProxy(url)
+
+	// Update the headers to allow for SSL redirection
+	req.URL.Host = url.Host
+	req.URL.Scheme = url.Scheme
+	req.Header.Set("X-Forwarded-Host", req.Header.Get("Host"))
+	req.Host = url.Host
+
+	// Note that ServeHttp is non blocking and uses a go routine under the hood
+	proxy.ServeHTTP(res, req)
+}
+
 // Given a request send it to the appropriate url
 func handleRequestAndRedirect(res http.ResponseWriter, req *http.Request) {
 	requestPayload := parseRequestBody(req)
 	url := getProxyUrl(requestPayload.ProxyCondition)
+
 	logRequestPayload(requestPayload, url)
-  // more still to come...
+
+	serveReverseProxy(url, res, req)
 }
 
 func main() {
