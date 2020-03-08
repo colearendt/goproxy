719191f89ccd8531cb5fc29fab21c2526c80c207
Cole Arendt <cole.arendt@outlook.com>
2020-03-08 07:21:36 -0400

    try to pass port parameter directly

--- src/go/src/main/main.c
+++ src/go/src/main/main.c
@@ -1,7 +1,14 @@
 #include "_cgo_export.h"
 #include <R.h>
 #include <Rinternals.h>
+#include <stdlib.h>
+#include <string.h>
 
-SEXP theproxy() {
-  return Rf_ScalarInteger( runProxy() );
+// #define SHORT_VEC_LENGTH(x) (((VECSEXP) (x))->vecsxp.length)
+
+SEXP theproxy(SEXP port) {
+  SEXP sx = STRING_ELT(port, 0);
+  
+  GoString h = { (char*) CHAR(sx), 1 };
+  return Rf_ScalarInteger( runProxy(h) );
 }
