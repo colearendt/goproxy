501a2fb4129362832766ab3c6684e445b3772802
Cole Arendt <cole.arendt@outlook.com>
2020-03-08 07:21:18 -0400

    try to pass port parameter directly

--- src/go/src/main/main.go
+++ src/go/src/main/main.go
@@ -23,18 +23,17 @@ func getEnv(key, fallback string) string {
 }
 
 // Get the port to listen on
-func getListenAddress() string {
-	port := getEnv("PORT", "1338")
+func getListenAddress(port string) string {
 	return ":" + port
 }
 
 // Log the env variables required for a reverse proxy
-func logSetup() {
+func logSetup(port string) {
 	a_condtion_url := os.Getenv("A_CONDITION_URL")
 	b_condtion_url := os.Getenv("B_CONDITION_URL")
 	default_condtion_url := os.Getenv("DEFAULT_CONDITION_URL")
 
-	log.Printf("Server will run on: %s\n", getListenAddress())
+	log.Printf("Server will run on: %s\n", getListenAddress(port))
 	log.Printf("Redirecting to A url: %s\n", a_condtion_url)
 	log.Printf("Redirecting to B url: %s\n", b_condtion_url)
 	log.Printf("Redirecting to Default url: %s\n", default_condtion_url)
@@ -138,13 +137,13 @@ func handleRequestAndRedirect(res http.ResponseWriter, req *http.Request) {
 
 
 //export runProxy
-func runProxy() int {
+func runProxy(port string) int {
   // Log setup values
-	logSetup()
+	logSetup(port)
 
 	// start server
 	http.HandleFunc("/", handleRequestAndRedirect)
-	if err := http.ListenAndServe(getListenAddress(), nil); err != nil {
+	if err := http.ListenAndServe(getListenAddress(port), nil); err != nil {
 		panic(err)
 	}
 	return 1
