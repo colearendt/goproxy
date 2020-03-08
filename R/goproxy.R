d702b859b5f001cc174670e61861ead85964367c
Cole Arendt <cole.arendt@outlook.com>
2020-03-08 14:05:46 -0400

    update docs slightly

--- R/goproxy.R
+++ R/goproxy.R
@@ -1,7 +1,10 @@
-#' Doubles an integer using go
+#' Run a reverse proxy
+#' 
+#' @param url The url (with protocol) to proxy
+#' @param port The port on which to listen
 #'
 #' @useDynLib goproxy
 #' @export
-run_proxy <- function(port, url) {
+run_proxy <- function(url, port) {
   .Call("theproxy", port, url, PACKAGE = "goproxy")
 }
