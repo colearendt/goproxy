af3435098dfb316d535076cbd0ec4c0e478270bf
Cole Arendt <cole.arendt@outlook.com>
2020-03-09 07:28:00 -0400

    make run_proxy more friendly

--- R/goproxy.R
+++ R/goproxy.R
@@ -1,10 +1,71 @@
-#' Run a reverse proxy
+#' @useDynLib goproxy, .registration = TRUE
+"_PACKAGE"
+
+
+run_proxy_raw <- function(url, port = "9845") {
+  #warning("This is currently not interruptible. Please use `run_proxy()` instead")
+  
+  .Call("theproxy", port, url, PACKAGE = "goproxy")
+}
+
+#' Run a reverse proxy in a sub-process
 #' 
 #' @param url The url (with protocol) to proxy
 #' @param port The port on which to listen
-#'
-#' @useDynLib goproxy
+#' 
 #' @export
-run_proxy <- function(url, port) {
-  .Call("theproxy", port, url, PACKAGE = "goproxy")
+run_proxy <- function(url, port = "9845", timeout = 20) {
+  
+  message(glue::glue("Starting GoProxy on http://127.0.0.1:{port}"))
+  
+  server_proc <- callr::r_bg(
+    function(url, port) goproxy:::run_proxy_raw(url, port),
+    args = list(
+      url = url,
+      port = port
+    )
+  )
+  
+  # ensure process cleans up after itself
+  on.exit(server_proc$kill_tree(), add = TRUE)
+  
+  # max 20 seconds wait
+  ready <- FALSE
+  message("Waiting for proxy to start...")
+  for (i in 1:(timeout * 10)) {
+    Sys.sleep(0.1)
+    
+    if (!server_proc$is_alive()) {
+      stop(noop_print(server_proc$read_error_lines()))
+    }
+    
+    try_to_connect <- try(httr::GET(glue::glue("http://127.0.0.1:{port}")), silent = TRUE)
+    if (!inherits(try_to_connect, "try-error")) {
+      ready <- TRUE
+      break
+    }
+  }
+  
+  
+  if (!ready) stop(glue::glue("ERROR: GoProxy did not start after {timeout} seconds"))
+  
+  message("Started! Opening browser")
+  
+  if (rstudioapi::isAvailable()) rstudioapi::viewer(glue::glue("http://127.0.0.1:{port}"))
+  
+  # poll and watch
+  while(server_proc$is_alive()) {
+    Sys.sleep(0.1)
+    
+    noop_print(server_proc$read_output_lines())
+    noop_print(server_proc$read_error_lines())
+  }
+  invisible()
+}
+
+
+noop_print <- function(input){
+  if (length(input) > 0) {
+    message(paste(input, collapse = "\n"))
+  }
 }
